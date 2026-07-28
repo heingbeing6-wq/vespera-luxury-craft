7f3d7ca4adc6193eae4523fbfc60fb94b221ff9d
heingbeing6-wq <heingbeing6@gmail.com>
2026-07-28 18:59:20 +0500

    Update Vite config to include new plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { nitro } from 'nitro/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
+  plugins: [
+    tsconfigPaths(),
+    tanstackStart(),
+    nitro(),
+    viteReact()
+  ],
+});
