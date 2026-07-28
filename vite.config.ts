f9720c8901493aaf8fb4e6cc6c97b42b4e7d879e
heingbeing6-wq <heingbeing6@gmail.com>
2026-07-28 21:26:42 +0500

    Add Tailwind CSS plugin and update build config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { nitro } from 'nitro/vite';
+import tailwindcss from '@tailwindcss/vite'; 
 
 export default defineConfig({
   plugins: [
+    tailwindcss(), 
     tsconfigPaths(),
     tanstackStart(),
     nitro(),
     viteReact()
   ],
+  build: {
+    cssMinify: 'esbuild', 
+  },
 });
